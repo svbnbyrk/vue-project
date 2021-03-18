4cbe8a03231e044e22d292352be6b0879ace152d
svbnbyrk <svbnbyrk@gmail.com>
2021-03-18 23:56:23 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# my-project
+# vue-project
 
 ## Project setup
 ```
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+    <Button text="Update Task" color="blue" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "Hello default"
+    }
+  },
+  components: {
+    Button
+  }
+};
+</script>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
